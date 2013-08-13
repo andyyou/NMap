c97e2f353113d81e19b0b1ad3148d148ea9cb85a
AndyYou <andyyu0920@gmail.com>
2013-08-13 11:39:17 +0800

    first commit

---
+++ NMap/MapWindow.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace NMap
+{
+    partial class MapWindow
+    {
+        /// <summary> 
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 元件設計工具產生的程式碼
+
+        /// <summary> 
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改這個方法的內容。
+        ///
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lb = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lb
+            // 
+            this.lb.AutoSize = true;
+            this.lb.Location = new System.Drawing.Point(50, 60);
+            this.lb.Name = "lb";
+            this.lb.Size = new System.Drawing.Size(65, 12);
+            this.lb.TabIndex = 0;
+            this.lb.Text = "TEST plugin";
+            // 
+            // MapWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.lb);
+            this.Name = "MapWindow";
+            this.Size = new System.Drawing.Size(650, 848);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lb;
+    }
+}
---
+++ NMap/MapWindow.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using WRPlugIn;
+
+namespace NMap
+{
+    public partial class MapWindow : UserControl, IWRPlugIn, IWRMapWindow
+    {
+        public MapWindow()
+        {
+            InitializeComponent();
+        }
+
+        #region IWRMapWindow 成員
+
+        public void GetMapControlHandle(out IntPtr hndl)
+        {
+            hndl = Handle;
+        }
+
+        public void SetMapPosition(int w, int h)
+        {
+            SetBounds(0, 0, w, h);
+        }
+
+        #endregion
+
+        #region IWRPlugIn 成員
+
+        public void GetControlHandle(out IntPtr hndl)
+        {
+            hndl = Handle;
+        }
+
+        public void GetName(e_Language language, out string Name)
+        {
+            Name = "New Map";
+        }
+
+        public void Initialize(string unitsXMLPath)
+        {
+            // No Imemented;
+        }
+
+        public void SetPosition(int w, int h)
+        {
+            SetBounds(0, 0, w, h);
+        }
+
+        public void Unplug()
+        {
+            // No Imemented;
+        }
+
+        #endregion
+    }
+}
